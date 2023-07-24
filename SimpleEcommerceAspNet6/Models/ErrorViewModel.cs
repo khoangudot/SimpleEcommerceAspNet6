a623e8ceea32c48536b4e10257dae42eee4ff437
khoangudot <86679522+khoangudot@users.noreply.github.com>
2023-07-24 16:13:47 +0700

    CreateProject,Add Pakges

---
+++ SimpleEcommerceAspNet6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SimpleEcommerceAspNet6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
